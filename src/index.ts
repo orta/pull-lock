b46445979877462af1d4967886508491bf4cdbb4
xtina-starr <c.thompsonms@gmail.com>
2018-09-27 18:26:59 -0400

    clean up script

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,4 @@
 import { exec } from "child_process"
 
 const diffCommand = `git diff-tree -r --name-only --no-commit-id ORIG_HEAD HEAD`
-exec(diffCommand, (err, stdout, stderr) => {
-  console.log("hello world")
-  console.log("stdout:", stdout)
-  console.log("stderr:", stderr)
-})
+exec(diffCommand, (err, stdout, stderr) => stdout)
